91580d829fc830d28de190f0a91c34d6cb11e855
PomboObeso <matheus.gomes201@gmail.com>
2023-01-28 20:41:31 -0300

    importações + assets + inicio da header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import './components/Header';
+import Header from './components/Header';
 
 function App() {
   return (
-    <p>MEU DEUX</p>
+    <Header/>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul className='header__lista'>
+                   <li className='header__item'>
+                        <h1 className='logotipo'>SEPLAG </h1>
+                   </li>
+                   <li className='header__item'>
+                        <a href='#'>HOME</a>
+                   </li>
+                   <li className='header__item'>
+                        <a href='#'>GALERIA</a>
+                   </li>
+                   <li className='header__item'>
+                        <a href='#'>NOTÍCIAS</a>
+                   </li> 
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
