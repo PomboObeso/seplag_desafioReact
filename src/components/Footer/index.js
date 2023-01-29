0bd45093e72045dad8f3ba69b78309313b0c739b
PomboObeso <matheus.gomes201@gmail.com>
2023-01-28 21:10:10 -0300

    footer - completo

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='rodape'>
+      Copyright &reg; 2022 SEPLAG - Secretária do Estado de Planejamento, Gestão e Patrimônio | Todos os direitos reservados
+    </footer>
+  )
+}
+
+export default Footer;
