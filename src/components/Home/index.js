648fdccd613fff4d393a309df1951ce77200f8de
PomboObeso <matheus.gomes201@gmail.com>
2023-01-29 17:39:38 -0300

    cards noticias - css completo (falta alinhar)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,15 +16,43 @@ const Home = () => {
                     <img src='./img/anuncio1.png' alt='t1'/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src='./img/anuncio1.png' alt='t1'/>
+                    <img src='./img/anuncio2.png' alt='t1'/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src='./img/anuncio1.png' alt='t1'/>
+                    <img src='./img/anuncio3.png' alt='t1'/>
                 </SwiperSlide>
             </Swiper>
 
             <section className='home__noticias'>
-                
+                <h2>Últimas notícias</h2>
+                <div className='home__card'>
+                    <div className='card__cabecalho'>
+                        <h3>Feriado à vista!</h3>
+                        <p>22/06/2022</p>
+                    </div>
+                    <p className='card__conteudo'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu molestie est, ac sollicitudin ligula. 
+                    Curabitur nec enim et justo aliquam tristique ac eget orci. Quisque tempor neque nibh, pellentesque rutrum orci commodo quis. 
+                    Curabitur volutpat erat et elit rutrum ornare. 
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                    Pellentesque laoreet sem nisi, in viverra metus euismod et. Aenean a eros id diam consequat sodales.
+                    </p>
+                    <a href='#' className='home__btn'>Ver mais</a>
+                </div>
+                <div className='home__card'>
+                    <div className='card__cabecalho'>
+                        <h3>Novo curso Egal!</h3>
+                        <p>20/06/2022</p>
+                    </div>
+                    <p className='card__conteudo'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu molestie est, ac sollicitudin ligula. 
+                    Curabitur nec enim et justo aliquam tristique ac eget orci. Quisque tempor neque nibh, pellentesque rutrum orci commodo quis. 
+                    Curabitur volutpat erat et elit rutrum ornare. 
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                    Pellentesque laoreet sem nisi, in viverra metus euismod et. Aenean a eros id diam consequat sodales.
+                    </p>
+                    <a href='#' className='home__btn'>Ver mais</a>
+                </div>
             </section>
         </div>
     );
