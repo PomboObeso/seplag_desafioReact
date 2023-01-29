fc2e6f5c878bd6ec95ce256ad4afddf2eae2925e
PomboObeso <matheus.gomes201@gmail.com>
2023-01-29 01:24:55 -0300

    box shadow - aside itens

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -5,62 +5,62 @@ const Aside = () => {
     return(
         <aside className='menu__lateral'>
             <ul className='menu__lista'>
-                <li className='menu__item'>
+                <li className='menu__item-user'>
                     <div className='menu__item-usuario'>
                         <img src='./img/user_log.png' alt='Foto perfil'/>
                         <div className='menu__item--info'>
                             <h2>Usuário Teste</h2>
                             <p>Editar Perfil</p>
                         </div>
-                        <img src='./icons/logout.svg' alt='Logout'/>
+                        <img className='logout' src='./icons/logout.svg' alt='Logout'/>
                     </div>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/headset_mic.svg' alt='Suporte STI'/>
+                    <img className='menu__icon' src='./icons/headset_mic.svg' alt='Suporte STI'/>
                     <h3>Central de Serviços - STI</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/headset_mic.svg' alt='Suporte SEI'/>
+                    <img className='menu__icon' src='./icons/headset_mic.svg' alt='Suporte SEI'/>
                     <h3>Central de Serviços - SEI</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/newspaper.svg' alt='Diário Oficial'/>
+                    <img className='menu__icon' src='./icons/newspaper.svg' alt='Diário Oficial'/>
                     <h3>Diário Oficial</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/insert_chart.svg' alt='Dados Alagoas'/>
+                    <img className='menu__icon' src='./icons/insert_chart.svg' alt='Dados Alagoas'/>
                     <h3>Dados AL</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/email.svg' alt=''/>
+                    <img className='menu__icon' src='./icons/email.svg' alt=''/>
                     <h3>Expresso Livre</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/desktop_windows.svg' alt='Facilita'/>
+                    <img className='menu__icon' src='./icons/desktop_windows.svg' alt='Facilita'/>
                     <h3>Facilita</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/description.svg' alt='Integra'/>
+                    <img className='menu__icon' src='./icons/description.svg' alt='Integra'/>
                     <h3>Integra</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/medical_services.svg' alt='Perícia Médica'/>
+                    <img className='menu__icon' src='./icons/medical_services.svg' alt='Perícia Médica'/>
                     <h3>Perícia Médica</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/people_outline.svg' alt='Portal de Servidor'/>
+                    <img className='menu__icon' src='./icons/people_outline.svg' alt='Portal de Servidor'/>
                     <h3>Portal do Servidor</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/workspace_premium.svg' alt='Qualidade'/>
+                    <img className='menu__icon' src='./icons/workspace_premium.svg' alt='Qualidade'/>
                     <h3>Qualidade</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/video_camera_front.svg' alt='Salas de reunião'/>
+                    <img className='menu__icon' src='./icons/video_camera_front.svg' alt='Salas de reunião'/>
                     <h3>Salas de reunião</h3>
                 </li>
                 <li className='menu__item'>
-                    <img src='./icons/perm_contact_calendar.svg' alt='Portal SEI'/>
+                    <img className='menu__icon' src='./icons/perm_contact_calendar.svg' alt='Portal SEI'/>
                     <h3>Portal SEI</h3>
                 </li>
             </ul>
