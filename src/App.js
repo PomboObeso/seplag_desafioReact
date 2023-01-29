df3656b7af036a739fadd6b217d7609dc09657cc
PomboObeso <matheus.gomes201@gmail.com>
2023-01-28 22:26:23 -0300

    subfooter - logo do estado + midias sociais

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Footer from './components/Footer';
 import './components/Header';
 import Header from './components/Header';
+import SubFooter from './components/SubFooter';
 
 function App() {
   return (
     <>
       <Header/>
+      <SubFooter/>
       <Footer/>
     </>
   );
---
+++ src/components/SubFooter/index.js
@@ -0,0 +1,17 @@
+import './SubFooter.css';
+
+const SubFooter = () => {
+    return (
+        <section className='subfooter'>
+            <img src='./img/logo-estado.png' alt='Logotipo do Estado'/>
+            <div className='midias-sociais'>
+                <img src='./img/midia-social1.svg' alt='Icone do Estado'/>
+                <img src='./img/midia-social3.svg' alt='Icone do Estado'/>
+                <img src='./img/midia-social2.svg' alt='Icone do Estado'/>
+                <p>seplagalagoas</p>
+            </div>
+        </section>
+    );
+}
+
+export default SubFooter;
